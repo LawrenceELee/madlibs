173bce3eda3911a63746057940c1e4a6d88f8965
Lawrence L <Lawrence.E.Lee@gmail.com>
2023-02-12 18:47:35 -0800

    feat: Add logic to form to display information submitted to form.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 //import logo from './logo.svg'; //used by the default react sample webpage
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
 import './App.css';
 
 let madlib = "Hello, ";
 		//"It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, ___(NAME)___’s friends were willing to share their ___(FOOD)___ with her.";
 
+const formReducer = (state, event) => {
+	return {
+		...state,
+		[event.name]: event.value
+	}
+}
 
 function App() {
+
+	const [formData, setFormData] = useReducer(formReducer, {});
 
 	//simulate the delay from fetaching/submitting to an API
 	const [submitting, setSubmitting] = useState(false);
@@ -27,6 +35,13 @@ function App() {
 		*/
 	}
 
+	const handleChange = event => {
+		setFormData({
+			name: event.target.name,
+			value: event.target.value,
+		});
+	}
+
 	return (
 			<div className="everything">
 			<h1>Mad Libs</h1>
@@ -34,14 +49,24 @@ function App() {
 				<fieldset>
 					<label>
 						<p>First Name:</p>
-						<input name="firstname" />
+						<input name="firstname" onChange={handleChange}/>
 					</label>
 				</fieldset>
 				<button type="submit">Create Mad Lib!</button>
 			</form>
 
 			{/*conditionally display message when submitting*/}
-			<p>{submitting && <div> Submitting Mad Lib...</div>}</p>
+			<p>{submitting && 
+				<div> Submitting Mad Lib...
+				<p>
+				You submitted:
+				<ul>
+					{Object.entries(formData).map(([name, value]) => (
+						<li key={name}><strong>{name}</strong>:{value.toString()}</li>
+					))}
+				</ul>
+				</p>
+				</div>}</p>
 			</div>
 	);
 	/*
